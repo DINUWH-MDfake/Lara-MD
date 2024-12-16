62cf6009b63c6036707e3a18411c8b7505539d19
DINUWH-MDfake <dinuwahmd@gmail.com>
2024-12-16 20:46:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "nNRUgAIB#gsPfwhZEyy17uQ5-W7ICn7zAGnE_PsnlaU-2Y4HkyCc",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
+SESSION_ID: process.env.SESSION_ID || "levanter_1650d51892c23847129be84f892b97db3f",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",    
 };
